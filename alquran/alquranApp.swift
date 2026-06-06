161cf2934217a0e8edc749c977507dddfe306cf8
irsalshydiq <ichalprov@gmail.com>
2026-06-06 22:39:23 +0700

    Initial Commit

---
+++ alquran/alquranApp.swift
@@ -0,0 +1,17 @@
+//
+//  alquranApp.swift
+//  alquran
+//
+//  Created by mac on 06/06/26.
+//
+
+import SwiftUI
+
+@main
+struct alquranApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
